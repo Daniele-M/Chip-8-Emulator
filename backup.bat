3234ab4f61f5e3ec41859695329bf506fad17065
Daniele-M <da.massanti@gmail.com>
2026-03-31 16:46:31 +0200

    backup: 2026-31-03_1646

--- backup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set mydate=%%c-%%a-%%b
-for /f "tokens=1-2 delims=: " %%a in ("%time%") do set mytime=%%a%%b
-set timestamp=%mydate%_%mytime%
-git add .
-git commit -m "backup: %timestamp%"
-git push origin main
-pause
